150d09077e1d6bb97d2551cf460624aa8c42fa71
Alejandro Hernandez <alejandrojaez@outlook.com>
2023-08-13 17:52:00 -0600

    first commit

--- client.go
+++ client.go
@@ -3,6 +3,8 @@ package dhru
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/tidwall/gjson"
 	"io"
 	"log/slog"
@@ -17,30 +19,44 @@ type AccountInfo struct {
 	Currency  string
 }
 
-func call(serverUrl string, username string, apikey string, action string) *http.Response {
+func call(serverUrl string, username string, apikey string, action string) (*http.Response, error) {
 	data := url.Values{
 		"username":      {username},
 		"apiaccesskey":  {apikey},
 		"requestformat": {"JSON"},
 		"action":        {action},
 	}
+
 	response, err := http.PostForm(serverUrl, data)
-	if err != nil {
-		slog.Error("", err)
+
+	if err != nil || response.StatusCode != 200 {
+		switch response.StatusCode {
+		case 404:
+			return response, errors.New(fmt.Sprintf("StatusCode=404, %s not found", serverUrl))
+		default:
+			return response, errors.New(fmt.Sprintf("StatusCode=%d, %s not found", response.StatusCode, response.Status))
+		}
 	}
-	return response
+	return response, err
 }
 
 func GetAccountInfo(serverUrl string, username string, apikey string) (AccountInfo, error) {
 	var accountInfo AccountInfo
 	var err error
-	response := call(serverUrl, username, apikey, "accountinfo")
+	response, err := call(serverUrl, username, apikey, "accountinfo")
+	if err != nil {
+		return AccountInfo{}, err
+	}
 	bodyResponse, err := io.ReadAll(response.Body)
 	if err != nil {
 		return AccountInfo{}, err
 	}
 
 	filteredJSON := gjson.Get(string(bodyResponse), "SUCCESS.0.AccountInfo")
+	if filteredJSON.Type != gjson.JSON {
+		errorJSON := gjson.Get(string(bodyResponse), "ERROR.0.MESSAGE")
+		return accountInfo, errors.New(errorJSON.Str)
+	}
 	err = json.NewDecoder(bytes.NewReader([]byte(filteredJSON.Raw))).Decode(&accountInfo)
 	if err != nil {
 		return AccountInfo{}, err
--- client_test.go
+++ client_test.go
@@ -1,13 +1,30 @@
 package dhru
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
 	"testing"
 )
 
+func TestMain(m *testing.M) {
+	err := godotenv.Load(".env.test")
+	if err != nil {
+		log.Fatal("Error loading .env.test file")
+	}
+	// call flag.Parse() here if TestMain uses flags
+	os.Exit(m.Run())
+}
+
 func TestGetAccountInfo(t *testing.T) {
-	acInfo, err := GetAccountInfo("https://www.movilunlock.com/api/index.php", "taoog", "FTX-6RT-VG5-MKB-K3P-5AS-KQZ-03T")
+	url := os.Getenv("DHRU_SERVER_URL")
+	username := os.Getenv("DHRU_USERNAME")
+	key := os.Getenv("DHRU_API_KEY")
+	acInfo, err := GetAccountInfo(url, username, key)
 	notWant := AccountInfo{}
 	if acInfo == notWant || err != nil {
 		t.Fatalf("error: %s", err)
 	}
+	fmt.Printf("%#v\n", acInfo)
 }
