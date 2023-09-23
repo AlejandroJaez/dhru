d85b90a6e94085de399aaa87fc5841dd46429e14
Alejandro Hernandez <alejandrojaez@outlook.com>
2023-09-22 18:23:52 -0600

    add test for account info

--- client_test.go
+++ client_test.go
@@ -1,9 +1,21 @@
 package dhru_test
 
 import (
+	"fmt"
 	"github.com/AlejandroJaez/dhru"
 	"testing"
 )
+
+func TestGetAccountInfo(t *testing.T) {
+	t.Parallel()
+	accountInfo, err := dhru.GetAccountInfo("", "", "")
+
+	if err != nil {
+		t.Fatalf("error: %v", err)
+	}
+
+	fmt.Printf("Emp: %+v\n", accountInfo)
+}
 
 func TestGetAllServices(t *testing.T) {
 	t.Parallel()
